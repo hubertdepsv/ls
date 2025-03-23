d013fa365cc04a9a02d94cd96cfa9939d1f997de
Hubert de Parseval <hp98@hotmail.fr>
2025-03-23 12:45:28 +0000

    Tests and initial pages

--- py175/cms/app.py
+++ py175/cms/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Getting started"
+    root = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(root, "cms", "data")
+    files = [os.path.basename(path) for path in os.listdir(data_dir)]
+    return render_template('index.html', files=files)
+
+@app.route("/<filename>")
+def view_file(filename):
+    root = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(root, "cms", "data")
+    return send_from_directory(data_dir, filename)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
