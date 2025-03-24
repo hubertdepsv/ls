cbbe4f9a0bcb2b0ff889f7bdb42f8271b006d0ca
Hubert de Parseval <hp98@hotmail.fr>
2025-03-24 21:20:16 +0000

    Next steps

--- py175/cms/app.py
+++ py175/cms/app.py
@@ -1,20 +1,70 @@
-from flask import Flask, render_template, send_from_directory
 import os
+from markdown import markdown
+from flask import (
+    flash,
+    Flask,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    send_from_directory
+)
 
 app = Flask(__name__)
+app.secret_key='secret'
+
+def get_data_path():
+    if app.config['TESTING']:
+        return os.path.join(os.path.dirname(__file__), 'tests', 'data')
+    else:
+        return os.path.join(os.path.dirname(__file__), 'cms', 'data')
 
 @app.route("/")
 def index():
-    root = os.path.abspath(os.path.dirname(__file__))
-    data_dir = os.path.join(root, "cms", "data")
+    data_dir = get_data_path()
     files = [os.path.basename(path) for path in os.listdir(data_dir)]
     return render_template('index.html', files=files)
 
 @app.route("/<filename>")
-def view_file(filename):
-    root = os.path.abspath(os.path.dirname(__file__))
-    data_dir = os.path.join(root, "cms", "data")
-    return send_from_directory(data_dir, filename)
+def file_content(filename):
+    data_dir = get_data_path()
+    file_path = os.path.join(data_dir, filename)
+
+    if os.path.isfile(file_path):
+        if filename.endswith('.md'):
+            with open(file_path, 'r') as file:
+                content = file.read()
+            return render_template('markdown.html', content=markdown(content))
+        else:
+            return send_from_directory(data_dir, filename)
+    else:
+        flash(f"{filename} does not exist.")
+        return redirect(url_for('index'))
+    
+@app.route("/<filename>/edit")
+def edit_file(filename):
+    data_dir = get_data_path()
+    file_path = os.path.join(data_dir, filename)
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return render_template('edit.html', filename=filename, content=content)
+    else:
+        flash(f"{filename} does not exist.")
+        return redirect(url_for('index'))
+
+@app.route("/<filename>", methods=['POST'])
+def save_file(filename):
+    data_dir = get_data_path()
+    file_path = os.path.join(data_dir, filename)
+
+    content = request.form['content']
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+    flash(f"{filename} has been updated.")
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
--- py175/cms/tests/test_app.py
+++ py175/cms/tests/test_app.py
@@ -1,18 +1,32 @@
 import unittest
+import shutil
+import os
 from app import app
 
 class CMSTest(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.client = app.test_client()
+        self.data_path = os.path.join(os.path.dirname(__file__), 'data')
+        os.makedirs(self.data_path, exist_ok=True)
+
+    def tearDown(self):
+        shutil.rmtree(self.data_path, ignore_errors=True)
+
+    def create_document(self, name, content=""):
+        with open(os.path.join(self.data_path, name), 'w') as file:
+            file.write(content)
 
     def test_index(self):
+        self.create_document("about.md")
+        self.create_document("changes.txt")
+        
         response = self.client.get("/")
+        
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content_type, "text/html; charset=utf-8")
-        self.assertIn("about.txt", response.get_data(as_text=True))
+        self.assertIn("about.md", response.get_data(as_text=True))
         self.assertIn("changes.txt", response.get_data(as_text=True))
-        self.assertIn("history.txt", response.get_data(as_text=True))
 
     def test_file(self):
         with self.client.get('/history.txt') as response:
@@ -20,6 +34,48 @@ class CMSTest(unittest.TestCase):
             self.assertEqual(response.content_type, "text/plain; charset=utf-8")
             self.assertIn("Python 0.9.0 (initial release) is released.",
                             response.get_data(as_text=True))
+
+    def test_document_not_found(self):
+        with self.client.get("/notafile.ext") as response:
+            self.assertEqual(response.status_code, 302)
+
+        with self.client.get(response.headers['Location']) as response:
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("notafile.ext does not exist",
+                          response.get_data(as_text=True))
+
+        with self.client.get("/") as response:
+            self.assertNotIn("notafile.ext does not exist",
+                             response.get_data(as_text=True))
+            
+    def test_viewing_markdown_document(self):
+        self.create_document("about.md")
+        response = self.client.get('/about.md')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+        self.assertIn("<h1>This is my app.</h1>", response.get_data(as_text=True))
+
+    def test_editing_document(self):
+        self.create_document("changes.txt")
+        response = self.client.get("/changes.txt/edit")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("<textarea", response.get_data(as_text=True))
+        self.assertIn('<button type="submit"', response.get_data(as_text=True))
+
+    def test_updating_document(self):
+        self.create_document("changes.txt")
+        response = self.client.post("/changes.txt",
+                                    data={'content': "new content"})
+        self.assertEqual(response.status_code, 302)
+
+        follow_response = self.client.get(response.headers['Location'])
+        self.assertIn("changes.txt has been updated",
+                      follow_response.get_data(as_text=True))
+
+        with self.client.get("/changes.txt") as content_response:
+            self.assertEqual(content_response.status_code, 200)
+            self.assertIn("new content",
+                          content_response.get_data(as_text=True))
 
 if __name__ == "__main__":
     unittest.main()
